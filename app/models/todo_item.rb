97552843b16a375244d9b010ed988b01259d5689
Kris <buchowiecki.krzysztof@gmail.com>
2017-08-05 13:28:08 +0200

    Delete and Mark as complete funcionality added

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,12 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+
+  def completed?
+  	# one line version: !completed_at.blank?
+  	if self.completed_at != nil
+  		true
+  	else
+  		false
+  	end
+  end
 end
